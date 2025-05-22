086c91e1a621d359ec3ae8ba3b39cfdd97624524
Maxence Agullo <magullo35@gmail.com>
2025-05-22 11:36:34 +0200

    edited time out

--- client/src/pages/Navigation.tsx
+++ client/src/pages/Navigation.tsx
@@ -29,7 +29,7 @@ export const Navigation = () => {
       setTimeout(() => {
         setSlideAnimation(true)
         navigate("/");
-      }, 200);
+      }, 400);
     }
   }, [isPasswordCorrect]);
 
